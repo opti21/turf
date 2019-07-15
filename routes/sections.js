a6c04f9e12469f7a4aebbced71597705086bbc0a
Brandon Rowe <browe.media@gmail.com>
2019-07-14 20:23:30 -0500

    New things

--- routes/sections.js
+++ routes/sections.js
@@ -19,23 +19,40 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/add_section', async(req, res) => {
-  var section = await Section.findOne({ name: req.body.name });
-  if (!section) {
-    var newSection = new Section({
-      name: req.body.name,
-      jobs: []
-    })
-  
-    newSection.save(function (err, Section) {
-      if (err) {
-        return console.log(err);
-      } else {
-        res.status(200).send('Success')
-        console.log('Section Saved')
-      }
-    })
+  if (req.user) {
+    var section = await Section.findOne({ name: req.body.name });
+    if (!section) {
+      var newSection = new Section({
+        name: req.body.name,
+        jobs: []
+      })
+    
+      newSection.save(function (err, Section) {
+        if (err) {
+          return console.log(err);
+        } else {
+          res.status(200).send('Success')
+          console.log('Section Saved')
+        }
+      })
+    } else {
+      res.status(500).send('Section already exists')
+    }
   } else {
-    res.status(500).send('Section already exists')
+    res.redirect('/login')
+  }
+});
+
+router.get('/delete/:id', async(req, res) => {
+  if (req.user) {
+      await Section.deleteOne({ _id: req.params.id}).exec().then(
+        res.status(200).send('Section deleted')
+      ), function (err) {
+        console.error(err)
+        res.status(500).send('Error deleting section')
+      };
+  } else {
+    res.redirect('/login')
   }
 })
 
